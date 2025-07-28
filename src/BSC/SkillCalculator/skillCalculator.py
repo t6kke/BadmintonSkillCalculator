f0caab75679afe3509da94e75502c2b3af9f3d72
Martin <m.t6kke@gmail.com>
2025-07-28 15:56:10 +0300

    initial setup for SkillCalc_v2 for new ELO calculation that handles per player ELO changes

--- src/BSC/SkillCalculator/skillCalculator.py
+++ src/BSC/SkillCalculator/skillCalculator.py
@@ -99,3 +99,76 @@ class SkillCalc:
             print("Change:", new_ELO_two - self.team_two.ELO)
         self.team_one.ELO = new_ELO_one
         self.team_two.ELO = new_ELO_two
+
+
+class SkillCalc_v2():
+    def __init__(self, players_obj_dict, verbose = False):
+        self.verbose = verbose #TODO extend verbose information to stuff that might be relevant
+        self.k_factor = 32 #TODO there should be 2 K-Factor variables, one for winner and one loser. when we have implemented ELO confidence value. example high ELO user will not lose a lot of points if new team with base/low ELO beats them
+        self.players_obj_dict = players_obj_dict
+
+    def calculate(self, match):
+        #TODO list
+        # 1. calculated expected score for players in each player.
+        # 2. need to assing winner/loser variables for final elo calculations
+        # 3. for each player against each oponent player determin the ELO difference and modify k-factor accordingly
+        # 4. ELO calculation for each player.
+        # 5. return some logical data structure for new ELO values, expected output is the ammount ELO was changed, not the new ELO value
+
+        t_one_p_one_id = list(list(match.keys())[0].team_members_set)[0]
+        t_one_p_two_id = list(list(match.keys())[0].team_members_set)[1]
+        t_two_p_one_id = list(list(match.keys())[1].team_members_set)[0]
+        t_two_p_two_id = list(list(match.keys())[1].team_members_set)[1]
+
+        # setp 1.
+        current_ELO_for_t_one_p_one = self.players_obj_dict.get(t_one_p_one_id).ELO
+        expectation_for_t_one_p_one = 1/(1+10**((current_ELO_for_t_one_p_one - current_ELO_for_t_one_p_one) / 400))
+
+        current_ELO_for_t_one_p_two = self.players_obj_dict.get(t_one_p_two_id).ELO
+        expectation_for_t_one_p_two = 1/(1+10**((current_ELO_for_t_one_p_two - current_ELO_for_t_one_p_two) / 400))
+
+        current_ELO_for_t_two_p_one = self.players_obj_dict.get(t_two_p_one_id).ELO
+        expectation_for_t_two_p_one = 1/(1+10**((current_ELO_for_t_two_p_one - current_ELO_for_t_two_p_one) / 400))
+
+        current_ELO_for_t_two_p_two = self.players_obj_dict.get(t_two_p_two_id).ELO
+        expectation_for_t_two_p_two = 1/(1+10**((current_ELO_for_t_two_p_two - current_ELO_for_t_two_p_two) / 400))
+
+        # step 2.
+        result_for_team_one = None
+        result_for_team_two = None
+        if list(match.values())[0] > list(match.values())[1]:
+            result_for_team_one = 1
+            result_for_team_two = 0
+        else:
+            result_for_team_one = 0
+            result_for_team_two = 1
+
+        # step 3.
+        #TODO
+
+        # step 4.
+        #TODO
+
+        # step 5.
+        #TODO
+
+    #TODO analyze if k-factor scaling values should be external configuration.
+    def __KFactorUP(self, ELO_diff):
+        if ELO_diff > 200:
+            self.k_factor = 70
+        elif ELO_diff <= 200 and ELO_diff > 100:
+            self.k_factor = 52
+        elif ELO_diff <= 100 and ELO_diff > 20:
+            self.k_factor = 40
+        elif ELO_diff <= 20 and ELO_diff > 0:
+            self.k_factor = 32
+
+    def __KFactorDOWN(self, ELO_diff):
+        if ELO_diff > 200:
+            self.k_factor = 2
+        elif ELO_diff <= 200 and ELO_diff > 100:
+            self.k_factor = 12
+        elif ELO_diff <= 100 and ELO_diff > 20:
+            self.k_factor = 24
+        elif ELO_diff <= 20 and ELO_diff > 0:
+            self.k_factor = 32
